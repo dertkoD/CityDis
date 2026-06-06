cbcec93dc2435065f0f6bec254dab1a1d4a3c7e6
Dertko Daniil <64313607+dertkoD@users.noreply.github.com>
2026-06-06 12:07:51 +0300

    +

--- Assets/Scripts/TerrainType.cs
+++ Assets/Scripts/TerrainType.cs
@@ -7,7 +7,10 @@ public enum TerrainType
     Plain = 0,
     River = 1,
     Railroad = 2,
-
+	Forest = 3,
+	WaterBody = 4,
+	City = 5, 
+	Mountains = 6,
     // City placeholder. May only appear in the CENTER of a tile (never on an edge).
-    Empty = 3
+    Empty = 7
 }
